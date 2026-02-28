f13f786405ce6ce14e2b951f919691616356eb57
Prabej Ansari <parbejansariyt@gmail.com>
2026-02-28 15:29:16 +0545

    project completed

--- convex/users.ts
+++ convex/users.ts
@@ -27,11 +27,37 @@ export const getUser = query({
         userId: v.string(),
     },
     handler: async (ctx, args) => {
-        if(!args.userId) return null;
-        const user =  await ctx.db.query("users").filter((q) => q.eq(q.field("userId"), args.userId)).first();
+        if (!args.userId) return null;
+        const user = await ctx.db.query("users").filter((q) => q.eq(q.field("userId"), args.userId)).first();
 
-        if(!user) return null;
+        if (!user) return null;
 
         return user;
     }
 })
+
+export const upgradeToPro = mutation({
+    args: {
+        email: v.string(),
+        lemonSqueezyCustomerId: v.string(),
+        lemonSqueezyOrderId: v.string(),
+        amount: v.number(),
+    },
+    handler: async (ctx, args) => {
+        const user = await ctx.db
+            .query("users")
+            .filter((q) => q.eq(q.field("email"), args.email))
+            .first();
+
+        if (!user) throw new Error("User not found");
+
+        await ctx.db.patch(user._id, {
+            isPro: true,
+            proSince: Date.now(),
+            lemonSqueezyCustomerId: args.lemonSqueezyCustomerId,
+            lemonSqueezyOrderId: args.lemonSqueezyOrderId,
+        });
+
+        return { success: true };
+    },
+});
--- src/app/pricing/_components/UpgradeButton.tsx
+++ src/app/pricing/_components/UpgradeButton.tsx
@@ -3,13 +3,14 @@ import Link from "next/link";
 
 export default function UpgradeButton() {
   const CHEKOUT_URL =
-    "https://ytprogrammingstore.lemonsqueezy.com/buy/d459dddb-a233-4060-9e72-90a1a7740552";
+    "https://codecraftys.lemonsqueezy.com/checkout/buy/1d1268f6-8560-4693-9845-a5f4ae5582a6";
 
   return (
     <Link
       href={CHEKOUT_URL}
+      target="_blank"
       className="inline-flex items-center justify-center gap-2 px-8 py-4 text-white 
-        bg-gradient-to-r from-blue-500 to-blue-600 rounded-lg 
+        bg-linear-to-r from-blue-500 to-blue-600 rounded-lg 
         hover:from-blue-600 hover:to-blue-700 transition-all"
     >
       <Zap className="w-5 h-5" />
